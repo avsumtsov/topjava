f3b2211b3ba400502ee7e96f92fe4a1c38d90650
avsumtsov <andrey.sumtsov@bakerhughes.com>
2021-09-12 11:53:47 +0300

    Added second println to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -30,6 +30,7 @@ public class UserMealsUtil {
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
         System.out.println("TODO return filtered list with excess. Implement by cycles");
+        System.out.println("on branch HW0");
         return null;
     }
 
